ebfff9f4f800a062a330351ab789553ecbac99b3
liumin <liumin@vtstar.net>
2018-04-21 16:08:32 +0800

    remove optional imports

--- src/android/com/vsdata/cordova/plugin/ScanHelper.java
+++ src/android/com/vsdata/cordova/plugin/ScanHelper.java
@@ -1,13 +1,12 @@
 package com.vsdata.cordova.plugin;
 
-import com.cus.scantest.R;
-
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.provider.Settings;
+
 public class ScanHelper {
 	/** scan settings show 0 : no; 1 : yes*/
 	public static final String SCAN_SETTINGS_HAVED = "scan_settings_haved";
